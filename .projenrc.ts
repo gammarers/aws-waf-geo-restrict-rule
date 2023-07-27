52a27d84b85914bbe641c9856aa5c98271bc49ba
yicr <yicr@users.noreply.github.com>
2023-07-27 15:01:22 +0900

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,16 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'yicr',
+  authorAddress: 'yicr@users.noreply.github.com',
+  cdkVersion: '2.62.0',
+  defaultReleaseBranch: 'main',
+  name: '@gammarer/aws-waf-geo-restriction-rule-group',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/yicr/aws-waf-geo-restriction-rule-group.git',
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+  releaseToNpm: false,
+});
+project.synth();
---
+++ README.md
@@ -0,0 +1,1 @@
+# AWS WAF(v2) GEO Restriction Rule Group
