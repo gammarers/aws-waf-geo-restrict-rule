00478377647ddfd25db5e5e023d42b25f89d996b
yicr <yicr@users.noreply.github.com>
2023-07-28 15:12:12 +0900

    chore: update deps upgrade option, auto approve option, release to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,23 @@
-import { awscdk } from 'projen';
+import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.62.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-waf-geo-restriction-rule-group',
+  description: 'This is an AWS CDK Construct for Geo Restriction Rule Group on WAF V2',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/yicr/aws-waf-geo-restriction-rule-group.git',
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-  releaseToNpm: false,
+  releaseToNpm: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * *']),
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yicr'],
+  },
 });
 project.synth();
