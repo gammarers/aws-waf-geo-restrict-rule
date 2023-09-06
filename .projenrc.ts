8c6da2dfdb5034e42bb9fe432bda414b61d3617c
yicr <yicr@users.noreply.github.com>
2023-09-06 11:38:03 +0900

    feat!: setting publish to maven

--- .projenrc.ts
+++ .projenrc.ts
@@ -27,5 +27,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
     distName: 'gammarer.aws-waf-geo-restriction-rule-group',
     module: 'gammarer.aws_waf_geo_restriction_rule_group',
   },
+  publishToMaven: {
+    mavenGroupId: 'com.gammarer',
+    javaPackage: 'com.gammarer.cdk.aws.waf_geo_restriction_rule_group',
+    mavenArtifactId: 'aws-waf-geo-restriction-rule-group',
+    mavenEndpoint: 'https://s01.oss.sonatype.org',
+  },
 });
 project.synth();
