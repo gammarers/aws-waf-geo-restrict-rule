6b40c244e323c0a5b5a45919ee436fba0ad1a6c1
yicr <yicr@users.noreply.github.com>
2024-01-15 18:09:55 +0900

    chore: workflow node version set to 20.11.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -14,7 +14,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/gammarer/aws-waf-geo-restriction-rule-group.git',
   npmAccess: javascript.NpmAccess.PUBLIC,
   minNodeVersion: '18.0.0',
-  workflowNodeVersion: '18.17.1',
+  workflowNodeVersion: '20.11.0',
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
