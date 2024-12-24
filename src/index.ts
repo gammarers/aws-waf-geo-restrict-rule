8ebdd31194b577c7d217df23248e0a60f8fb03fc
yicr <yicr@users.noreply.github.com>
2024-12-24 15:31:28 +0900

    feat!: rename classs & props &b more

--- src/index.ts
+++ src/index.ts
@@ -1,155 +1,56 @@
-import * as waf from 'aws-cdk-lib/aws-wafv2';
-import { Construct } from 'constructs';
+import * as wafv2 from 'aws-cdk-lib/aws-wafv2';
 
-export interface WafGeoRestrictRuleGroupProps {
-  readonly name?: string;
-  readonly scope: Scope;
+export interface RuleConfig {
+  readonly priority: number;
+  readonly ruleName?: string;
+  readonly cloudWatchMetricsName?: string;
+}
+
+export interface WAFGeoRestrictRuleProps {
   readonly allowCountries: string[];
-  readonly ipRateLimiting?: IpRateLimitingProperty;
-  readonly allowIpSetArn?: string;
 }
 
-export interface IpRateLimitingProperty {
-  readonly enable: boolean;
-  readonly count: number;
-}
+export class WAFGeoRestrictRule {
 
-export enum Scope {
-  GLOBAL = 'Global',
-  REGIONAL = 'Regional',
-}
+  constructor(private props: WAFGeoRestrictRuleProps) {}
 
-export class WafGeoRestrictRuleGroup extends waf.CfnRuleGroup {
-  constructor(scope: Construct, id: string, props: WafGeoRestrictRuleGroupProps) {
-    super(scope, id, {
-      name: props.name,
-      description: 'geo restrict rule group',
-      scope: ((): string => {
-        switch (props.scope) {
-          case Scope.GLOBAL:
-            return 'CLOUDFRONT';
-          case Scope.REGIONAL:
-            return 'REGIONAL';
-        }
-      })(),
-      capacity: 10,
-      rules: ((): Array<waf.CfnRuleGroup.RuleProperty> => {
-        const defaults: Array<waf.CfnRuleGroup.RuleProperty> = [
-          {
-            priority: 4,
-            name: 'allow-geo-rule',
-            action: {
-              allow: {},
-            },
-            visibilityConfig: {
-              cloudWatchMetricsEnabled: true,
-              sampledRequestsEnabled: true,
-              metricName: 'AllowGeoRule',
-            },
-            statement: {
-              geoMatchStatement: {
-                countryCodes: props.allowCountries,
-              },
-            },
-          },
-          {
-            priority: 5,
-            name: 'deny-geo-rule',
-            action: {
-              block: {
-                CustomResponse: {
-                  CustomResponseBodyKey: 'geo-restrict',
-                  ResponseCode: 403,
-                },
-              },
-            },
-            visibilityConfig: {
-              cloudWatchMetricsEnabled: true,
-              sampledRequestsEnabled: true,
-              metricName: 'DenyGeoRule',
-            },
-            statement: {
-              notStatement: {
-                statement: {
-                  geoMatchStatement: {
-                    countryCodes: props.allowCountries,
-                  },
-                },
-              },
-            },
-          },
-        ];
-        if (props.ipRateLimiting) {
-          if (props.ipRateLimiting.enable) {
-            if (props.ipRateLimiting.count < 100) {
-              throw new Error('IP rate limiting count value needs to be above 100');
-            } else {
-              const rule: waf.CfnRuleGroup.RuleProperty = {
-                priority: 2,
-                name: 'ip-rate-limiting-rule',
-                action: {
-                  block: {
-                    CustomResponse: {
-                      CustomResponseBodyKey: 'ip-rate-restrict',
-                      ResponseCode: 429,
-                    },
-                  },
-                },
-                visibilityConfig: {
-                  cloudWatchMetricsEnabled: true,
-                  sampledRequestsEnabled: true,
-                  metricName: 'DenyIpLimitingRule',
-                },
-                statement: {
-                  rateBasedStatement: {
-                    aggregateKeyType: 'IP',
-                    limit: props.ipRateLimiting.count,
-                  },
-                },
-              };
-              defaults.push(rule);
-            }
-          }
-        }
-        if (props.allowIpSetArn) {
-          const rule: waf.CfnRuleGroup.RuleProperty = {
-            priority: 0,
-            name: 'allow-ip-set-rule',
-            action: { allow: {} },
-            visibilityConfig: {
-              sampledRequestsEnabled: true,
-              cloudWatchMetricsEnabled: true,
-              metricName: 'AllowIpSetRule',
-            },
-            statement: {
-              ipSetReferenceStatement: {
-                arn: props.allowIpSetArn,
-              },
-            },
-          };
-          defaults.push(rule);
-        }
-        return defaults;
-      })(),
-      customResponseBodies: {
-        ['geo-restrict']: {
-          contentType: 'TEXT_PLAIN',
-          content: 'Sorry, You Are Not Allowed to Access This Service.',
-        },
-        ['ip-rate-restrict']: {
-          contentType: 'TEXT_PLAIN',
-          content: 'Sorry, You Are Not Allowed to Access This Service.',
-        },
-        ['sample-restrict']: {
-          contentType: 'TEXT_PLAIN',
-          content: 'Sorry, You Are Not Allowed to Access This Service.',
+  alowRule(config: RuleConfig): wafv2.CfnWebACL.RuleProperty {
+    return {
+      name: config.ruleName || 'allow-geo-rule',
+      priority: config.priority,
+      action: { allow: {} },
+      statement: {
+        geoMatchStatement: {
+          countryCodes: this.props.allowCountries,
         },
       },
       visibilityConfig: {
         cloudWatchMetricsEnabled: true,
+        metricName: config.cloudWatchMetricsName || 'AllowGeoMetric',
         sampledRequestsEnabled: true,
-        metricName: 'GeoRestrictRule',
       },
-    });
+    };
+  }
+
+  blockRule(config: RuleConfig): wafv2.CfnWebACL.RuleProperty {
+    return {
+      name: config.ruleName || 'block-geo-rule',
+      priority: config.priority,
+      action: { block: {} },
+      statement: {
+        notStatement: {
+          statement: {
+            geoMatchStatement: {
+              countryCodes: this.props.allowCountries,
+            },
+          },
+        },
+      },
+      visibilityConfig: {
+        cloudWatchMetricsEnabled: true,
+        metricName: config.cloudWatchMetricsName || 'BlockGeoMetric',
+        sampledRequestsEnabled: true,
+      },
+    };
   }
 }
---
+++ test/rule.default.test.ts
@@ -0,0 +1,85 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as wafv2 from 'aws-cdk-lib/aws-wafv2';
+import { WAFGeoRestrictRule } from '../src';
+
+describe('Web ACL Rule Group Global Scope default testing', () => {
+
+  const app = new App();
+  const stack = new Stack(app, 'TestingStack');
+
+  const geoRestrictRule = new WAFGeoRestrictRule({
+    allowCountries: ['JP'],
+  });
+
+  new wafv2.CfnWebACL(stack, 'WebACL', {
+    defaultAction: { allow: {} },
+    scope: 'CLOUD_FRONT',
+    name: 'WebAclWithCustomRules',
+    visibilityConfig: {
+      cloudWatchMetricsEnabled: true,
+      metricName: 'WebAclMetric',
+      sampledRequestsEnabled: true,
+    },
+    rules: [
+      geoRestrictRule.alowRule({
+        priority: 1,
+      }),
+      geoRestrictRule.blockRule({
+        priority: 2,
+      }),
+    ],
+  });
+
+  const template = Template.fromStack(stack);
+
+  it('Should have WAF WebACL Rule', async () => {
+    template.hasResourceProperties('AWS::WAFv2::WebACL', {
+      Rules: [
+        {
+          Action: {
+            Allow: {},
+          },
+          Name: 'allow-geo-rule',
+          Priority: 1,
+          Statement: {
+            GeoMatchStatement: {
+              CountryCodes: ['JP'],
+            },
+          },
+          VisibilityConfig: {
+            CloudWatchMetricsEnabled: true,
+            MetricName: 'AllowGeoMetric',
+            SampledRequestsEnabled: true,
+          },
+        },
+        {
+          Action: {
+            Block: {},
+          },
+          Name: 'block-geo-rule',
+          Priority: 2,
+          Statement: {
+            NotStatement: {
+              Statement: {
+                GeoMatchStatement: {
+                  CountryCodes: ['JP'],
+                },
+              },
+            },
+          },
+          VisibilityConfig: {
+            CloudWatchMetricsEnabled: true,
+            MetricName: 'BlockGeoMetric',
+            SampledRequestsEnabled: true,
+          },
+        },
+      ],
+    });
+  });
+
+  it('Should match snapshot', () => {
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+
+});
